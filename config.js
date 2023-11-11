e156e530510a70c5b866a896e3dfa8c63b3322ce
Salman6614 <135732288+Salman6614@users.noreply.github.com>
2023-11-11 10:28:18 +0500

    Update config.js

--- config.js
+++ config.js
@@ -10,17 +10,17 @@ global.email = 'sam@secktor.live'
 global.github = 'https://github.com/yasiyaofc1/FORZEN-MD'
 global.location = 'Sultanpur IN'
 global.gurl = 'https://instagram.com/' // add your username
-global.sudo = process.env.SUDO || '94760018802'
-global.devs = '94760018802';
+global.sudo = process.env.SUDO || '923011080145'
+global.devs = '923011080145';
 global.website = 'https://github.com/yasiyaofc1/FORZEN-MD' //wa.me/+94760018802
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/0b5a7839e8ee21874921e.jpg'
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://i.imgur.com/LqSFsGE.jpeg'
 module.exports = {
-  botname:   process.env.BOT_NAME === undefined ? 'FORZEN MD' : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'YASIYA OFC' : process.env.OWNER_NAME,
+  botname:   process.env.BOT_NAME === undefined ? '𝑆𝛥𝐿𝛭𝛥𝛮 MD' : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? '𝑆𝛥𝐿𝛭𝛥𝛮 OFC' : process.env.OWNER_NAME,
   sessionName:  process.env.SESSION_ID === undefined ? false : process.env.SESSION_ID,
-  author:  process.env.PACK_INFO.split(";")[0] === undefined ? 'YASIYA OFC' : process.env.PACK_INFO.split(";")[0],
+  author:  process.env.PACK_INFO.split(";")[0] === undefined ? '𝑆𝛥𝐿𝛭𝛥𝛮 OFC' : process.env.PACK_INFO.split(";")[0],
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
-  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? 'FORZEN MD' : process.env.PACK_INFO.split(";")[1],
+  packname:  process.env.PACK_INFO.split(";")[1] === undefined ? '𝑆𝛥𝐿𝛭𝛥𝛮 MD' : process.env.PACK_INFO.split(";")[1],
   autoreaction:  process.env.AUTO_REACTION  === undefined ? false : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
   alwaysonline:  process.env.ALWAYS_ONLINE === undefined ? false : process.env.ALWAYS_ONLINE,
